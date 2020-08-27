f8187893714a0f8413218d4d8ead346b7a005246
fm85 <famu1985@gmail.com>
2020-08-27 17:09:33 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""
+------------------------------------------------------------------
+Projekt:         MQTT Client Beispiel
+Beschreibung:    Publiziert exemlarisch einen Sensorwert über MQTT
+Abhängigkeiten:  paho-mqtt (vorher mit pip3 installieren)
+Institution:     GBS
+Verfasser:       F.Reifler
+Datum:           27.08.2020
+------------------------------------------------------------------
+"""
+
+import paho.mqtt.client as mqtt
+import Adafruit_DHT
+import socket
+import sys
+import time
+
+myClient = mqtt.Client()
+mySensorType = 11
+mySensorPin = 4
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code" + str(rc))
+
+def initMQTTClient():
+    myClient.on_connect = on_connect
+    keepalive = 60
+    myClient.connect("172.20.1.31",1883,keepalive)
+    myClient.loop_start()
+
+def getHostname():
+    return socket.gethostname()
+
+def start():
+    try:
+        initMQTTClient()
+        topic = getHostname() + "/temperature"
+        print("Publishing with the following Topic:" + topic)
+        while(True):
+            time.sleep(2)
+            humidity, temperature = Adafruit_DHT.read_retry(mySensorType, mySensorPin)
+            message = temerature
+            myClient.publish(topic,message)
+    except (KeyboardInterrupt, SystemExit):
+        print("Interrupted")
+        sys.exit(1)
+
+start()
